9712b9c97963c951c8e5f1f233ea2a5012acf382
Theodore Keloglou <zf@sirodoht.com>
2022-11-06 01:17:54 +0000

    rename project to alcman

--- alcman/wsgi.py
+++ alcman/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hania project.
+WSGI config for alcman project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hania.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alcman.settings")
 
 application = get_wsgi_application()
